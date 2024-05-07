8bd2639ec71f576475e08a113e31b0b71c26c627
HichamLuffy <imalexvssteve2020@gmail.com>
2024-05-07 11:51:11 +0100

    LFU

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,2 +1,36 @@
 #!/usr/bin/python3
+""" 100-lfu_cache.py """
 from base_caching import BaseCaching
+from collections import Counter
+
+
+
+class LFUCache(BaseCaching):
+    def __init__(self):
+        super().__init__()
+        self.key_access_times = Counter()
+        self.key_age = 0
+        self.key_order = {}
+
+    def put(self, key, item):
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                lfu_key, _ = self.key_access_times.most_common()[-1]
+                if self.key_order[lfu_key] == min(self.key_order.values()):
+                    print("DISCARD:", lfu_key)
+                    del self.cache_data[lfu_key]
+                    del self.key_order[lfu_key]
+                    del self.key_access_times[lfu_key]
+
+            self.cache_data[key] = item
+            self.key_access_times[key] += 1
+            self.key_age += 1
+            self.key_order[key] = self.key_age
+
+    def get(self, key):
+        if key is not None and key in self.cache_data:
+            self.key_access_times[key] += 1
+            self.key_age += 1
+            self.key_order[key] = self.key_age
+            return self.cache_data[key]
+        return None
