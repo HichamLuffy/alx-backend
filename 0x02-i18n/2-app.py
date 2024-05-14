bc69de628555f0ad4d785e6f475146844b9f7d8c
HichamLuffy <imalexvssteve2020@gmail.com>
2024-05-14 22:12:05 +0100

    task 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,1 +1,32 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" Flask app """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def index() -> str:
+    """Index route"""
+    return render_template("2-index.html")
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """doc doc doc"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
