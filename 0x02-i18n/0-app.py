90b2dee8b7afaca7f9e3b922f0bfa09139edecee
HichamLuffy <imalexvssteve2020@gmail.com>
2024-05-14 21:39:44 +0100

    basics

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 """Flask app"""
-from flask import Flask
-from flask import render_template as template
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
+    """Index route"""
     title = "Welcome to Holberton"
-    return template("0-index.html", title=title)
+    return render_template("0-index.html", title=title)
 
 
 if __name__ == "__main__":
