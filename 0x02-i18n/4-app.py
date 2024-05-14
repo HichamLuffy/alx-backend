edae4a11a5f4314e1a9ff41983245cc0944def05
HichamLuffy <imalexvssteve2020@gmail.com>
2024-05-14 22:40:56 +0100

    task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -28,7 +28,7 @@ def get_locale() -> str:
 @app.route("/")
 def index() -> str:
     """Index route"""
-    return render_template("3-index.html")
+    return render_template("4-index.html")
 
 
 if __name__ == "__main__":
