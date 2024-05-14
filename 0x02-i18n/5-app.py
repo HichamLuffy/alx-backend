2494fdeac0e09410dccef28fdf3b160728cdef8b
HichamLuffy <imalexvssteve2020@gmail.com>
2024-05-14 22:52:07 +0100

    fix prblm

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -50,7 +50,7 @@ def get_locale() -> str:
 @app.route("/")
 def index() -> str:
     """Index route"""
-    return render_template("4-index.html")
+    return render_template("5-index.html")
 
 
 if __name__ == "__main__":
